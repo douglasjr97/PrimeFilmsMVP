057337445f3020232da7e1e3f4e42975373efe56
Douglas Falcao <falcao.douglas97@gmail.com>
2022-05-20 22:55:40 -0300

    feat: get Api structure with window

--- src/FIlmes/index.tsx
+++ src/FIlmes/index.tsx
@@ -1,12 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import React from "react";
 import { Filme } from "../../interfaces/interface";
 
 export default function Filmes({ nome, foto, id, sinopse }: Filme) {
   return (
-    <View>
-      <Text>{nome}</Text>
-      <Text>{sinopse}</Text>
+    <View style={{}}>
+      <View style={styles.card}>
+        <Text style={styles.titulo}>{nome}</Text>
+
+        <Image source={{ uri: foto }} style={styles.capa} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "#FFF",
+    margin: 15,
+    elevation: 2,
+    marginTop: 50,
+  },
+
+  capa: {
+    height: 250,
+    zIndex: 2,
+  },
+  titulo: {
+    padding: 15,
+    fontSize: 18,
+    // fontWeight: "bold",
+  },
+});
