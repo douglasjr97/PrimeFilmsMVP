ed61133753f258f30528649359eba99be63f3722
Douglas Falcao <falcao.douglas97@gmail.com>
2022-05-22 22:34:11 -0300

    feat: refactor

--- App.tsx
+++ App.tsx
@@ -1,45 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  ActivityIndicator,
-} from 'react-native';
-import api from './services/api';
-import React, { useEffect, useState } from 'react';
-import Filmes from './src/FIlmes';
-import { Filme } from './interfaces/interface';
+import React from 'react';
+import Dashboard from './src/pages/Dashboard';
 
 export default function App() {
-  const [filmes, setFilmes] = useState<Filme[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function loadFilmes() {
-      const response = await api.get('/r-api/?api=filmes');
-      setFilmes(response.data);
-      setLoading(false);
-    }
-    loadFilmes();
-  }, []);
-
-  if (loading) {
-    return (
-      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-        <ActivityIndicator color="#2009ec" size={45} />
-      </View>
-    );
-  } else {
-    return (
-      <View>
-        <FlatList
-          data={filmes}
-          keyExtractor={(item) => String(item.id)}
-          renderItem={({ item }) => <Filmes {...item} />}
-        />
-        {/* <StatusBar style="auto" /> */}
-      </View>
-    );
-  }
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
 }
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,39 @@
+import { View, Text, ActivityIndicator, FlatList } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import api from '../../../services/api';
+import { Filme } from '../../../interfaces/interface';
+import Filmes from '../../FIlmes';
+
+const Dashboard = () => {
+  const [filmes, setFilmes] = useState<Filme[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get('/r-api/?api=filmes');
+      setFilmes(response.data);
+      setLoading(false);
+    }
+    loadFilmes();
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        <ActivityIndicator color="#2009ec" size={45} />
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        <FlatList
+          data={filmes}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <Filmes {...item} />}
+        />
+      </View>
+    );
+  }
+};
+
+export default Dashboard;
