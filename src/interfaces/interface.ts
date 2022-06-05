af06095764d99519b3fd2dc5966a74c150979204
Douglas Falcao <falcao.douglas97@gmail.com>
2022-06-05 13:48:29 -0300

    fix: typing Api without required

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -6,21 +6,21 @@
 // };
 
 export type ApiMovie = {
-  id: number;
-  title: string;
-  release_date: string;
-  poster_path: string;
-  backdrop_path: string;
-  vote_average: number;
-  overview: string;
+  id?: number;
+  title?: string;
+  release_date?: string;
+  poster_path?: string;
+  backdrop_path?: string;
+  vote_average?: number;
+  overview?: string;
 };
 
 export type Movie = {
-  id: number;
-  title: string;
-  releaseDate: string;
-  imagePosterUrl: string;
-  backdrop_path?: string;
-  voteAverage: number;
-  overview: string;
+  id?: number;
+  title?: string;
+  releaseDate?: string;
+  imagePosterUrl?: string;
+  backdropPath?: string;
+  voteAverage?: number;
+  overview?: string;
 };
